ba42c39dfadb8a92c5efaa58e2700513f289e928
Pankaj Ramesh Vasnani <pankaj.techuser@gmail.com>
2021-02-19 21:18:14 +0700

    Add validation for login input data

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use View, Auth;
+use View, Auth, Validator;
 
 class LoginController extends Controller
 {
@@ -19,22 +19,34 @@ class LoginController extends Controller
      * Function to validate login details
      */
     public function validateLoginForm(Request $request) {
-        $credentials = $request->only('email', 'password');
+        $validator = Validator::make(
+            $request->all(), 
+            [
+                'email' => 'required',
+                'password' => 'required'
+            ]
+        );
 
-        if (Auth::attempt($credentials)) {
-            return redirect()->route('dashboard');
+        if ($validator->fails()) {
+            return back()->withErrors([
+                'message' => 'Enter all the input values.',
+            ]);
+        } else {
+
+            $credentials = $request->only('email', 'password');
+
+            if (Auth::attempt($credentials)) {
+                return redirect()->route('dashboard');
+            }
+
+            return back()->withErrors([
+                'message' => 'The provided credentials do not match our records.',
+            ]);
         }
-
-        return back()->withErrors([
-            'message' => 'The provided credentials do not match our records.',
-        ]);
     }
 
     /**
-     * Log the user out of the application.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
+     * Function to logout the user
      */
     public function logout(Request $request) {
         Auth::logout();
